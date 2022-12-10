3eba66f2a76dd60a0b440c098331c65d60f17814
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-11 01:28:04 +0530

    Update y.py

--- y.py
+++ y.py
@@ -2,6 +2,10 @@ print("\nWELCOME TO ALPHA ? NONE\n\n")
 
 print("YASHVI? \n\n")
 
+print("SORRY FOR ENTERING INTO UR LIFE \n\n")
+
+print("MOVE ON\n\n")
+
 print("GOOD BYE 🙂\n\n")
 
 print("DEATH\n")
