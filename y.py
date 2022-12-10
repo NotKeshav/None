5d9e4801563e8bc0db4eddff6f17e715e105ceb1
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-11 01:35:32 +0530

    Update y.py

--- y.py
+++ y.py
@@ -4,7 +4,25 @@ print("YASHVI? \n\n")
 
 print("SORRY FOR ENTERING INTO UR LIFE \n\n")
 
-print("MOVE ON\n\n")
+print("I'M TOO MUCH TOXIC\n\n")
+
+print("I DON'T WANT ANY THIRD PERSON IN BETWEEN\n\n")
+
+print("LITERALLY LIKE A PSYCHO\n\n")
+
+print("NOT EVEN AS A FRIEND\n\n")
+
+print("I MIGHT BE WRONG ALWAYS\n\n")
+
+print("NO NEED TO WORRY ABOUT ME\n\n")
+
+print("ALL THE BEST\n\n")
+
+print("I CAN'T BE A PERSON LIKE WHOM U WANT\n\n")
+
+print("ENJOY YOUR LIFE\n\n")
+
+print("LIVE PEACEFULLY\n\n")
 
 print("GOOD BYE 🙂\n\n")
 
